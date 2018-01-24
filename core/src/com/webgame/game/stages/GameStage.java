e5a44659338a05e4e652f8b223c34fb94d4ae2fd
tfkfan <sylar131@live.ru>
2018-01-24 21:55:17 +0300

    Litle changes

--- core/src/com/webgame/game/stages/GameStage.java
+++ core/src/com/webgame/game/stages/GameStage.java
@@ -5,12 +5,16 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.webgame.game.world.WorldRenderer;
@@ -27,19 +31,19 @@ import java.util.List;
 import static com.webgame.game.Configs.VIEW_HEIGHT;
 import static com.webgame.game.Configs.PPM;
 
-public class GameStage extends Stage implements InputProcessor {
+public class GameStage extends Stage {
     public static final float TIME_STEP = 1 / 20f;
     private SpriteBatch batch;
     private World world;
     private WorldRenderer worldRenderer;
     protected ShapeRenderer sr;
 
-    private Player player;
+    private final Player player;
     private List<Player> enemies;
     private CollisionHandler clsnHandler;
 
 
-    public GameStage(){
+    public GameStage() {
         Gdx.input.setInputProcessor(this);
 
         batch = new SpriteBatch();
@@ -49,12 +53,11 @@ public class GameStage extends Stage implements InputProcessor {
         camera.update();
 
         world = new World(new Vector2(0, 0), true);
-        worldRenderer = new WorldRenderer(world , camera);
-        setViewport( new StretchViewport(VIEW_WIDTH / PPM, VIEW_HEIGHT / PPM, camera));
+        worldRenderer = new WorldRenderer(world, camera);
+        setViewport(new StretchViewport(VIEW_WIDTH / PPM, VIEW_HEIGHT / PPM, camera));
 
         player = new Mage(batch, "mage.png");
         player.createObject(world);
-
 
 
         Player enemy = new Knight(batch, "knight.png");
@@ -71,6 +74,23 @@ public class GameStage extends Stage implements InputProcessor {
 
         sr.setAutoShapeType(true);
 
+        player.setTouchable(Touchable.enabled);
+        player.addListener(new InputListener() {
+            @Override
+            public boolean keyDown(InputEvent event, int keycode) {
+                if (keycode >= 8 && keycode <= 16)
+                    player.setCurrentSkillIndex(keycode - 8);
+                return false;
+            }
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+               
+
+                return true;
+            }
+        });
+        setKeyboardFocus(player);
         this.addActor(player);
         this.addActor(enemy);
     }
@@ -94,7 +114,7 @@ public class GameStage extends Stage implements InputProcessor {
     }
 
     @Override
-    public void draw(){
+    public void draw() {
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -143,63 +163,20 @@ public class GameStage extends Stage implements InputProcessor {
         getCamera().position.y = player.getY();
     }
 
-
-    @Override
-    public boolean keyDown(int keycode) {
-        if(keycode >= 8 && keycode <=16)
-            player.setCurrentSkillIndex(keycode - 8);
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
     @Override
     public boolean touchDown(int x, int y, int pointer, int button) {
-
+        super.touchDown( x,  y,  pointer, button);
         if (button == Input.Buttons.LEFT) {
 
             Vector3 target = getCamera().unproject(new Vector3(x, y, 0), getViewport().getScreenX(), getViewport().getScreenY(),
-                   getViewport().getScreenWidth(), getViewport().getScreenHeight());
+                    getViewport().getScreenWidth(), getViewport().getScreenHeight());
 
             player.attack(target.x, target.y);
 
             return true;
         }
-        return false;
+        return true;
     }
 
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        // TODO Auto-generated method stub
-        return false;
-    }
 
 }
