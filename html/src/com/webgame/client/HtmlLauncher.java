d6c0ce6012104750a30a0e6f8ee9fab897de568b
tfkfan <sylar131@live.ru>
2018-04-21 20:13:41 +0300

    Some configs changes

--- html/src/com/webgame/client/HtmlLauncher.java
+++ html/src/com/webgame/client/HtmlLauncher.java
@@ -1,9 +1,8 @@
-package com.webgame.client.client;
+package com.webgame.client;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.webgame.client.WebGame;
 
 public class HtmlLauncher extends GwtApplication {
 
